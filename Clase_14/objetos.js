0b5da8b17fc2884349fc76312e0a6bbc354d35dd
vfigunn <vfigun@gmail.com>
2024-01-02 14:10:40 -0300

    Clase 14

---
+++ Clase_14/objetos.js
@@ -0,0 +1,17 @@
+//objetos -> estructura que nos permite describir una entidad, asociando sus propiedades y valores.
+//utilizan llaves
+
+/* sintaxis:
+   {
+        propiedad/key/clave:valor,
+        propiedad/key/clave:valor,
+        propiedad/key/clave:valor,
+        propiedad/key/clave:valor,
+   }
+*/
+
+{
+    marca:"Pepito",
+    precio:2222,
+    stock:10,
+}
